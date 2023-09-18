889dcb8551f14fe528cd73860109de8fb818579a
sarthaksarangi <sarthakrohan5@gmail.com>
2023-09-18 15:16:15 +0530

    added express

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-const eventEmitter = new EventEmitter();
+// const eventEmitter = new EventEmitter();
 
 
 
-eventEmitter.on('start', () => {
-    console.log('started');
-  });
+// eventEmitter.on('start', () => {
+//     console.log('started');
+//   });
   
+// d
+// eventEmitter.emit('start');
 
-eventEmitter.emit('start');
+// const EventEmitter = require('events');
+
+// const eventEmitter = new EventEmitter();
+
+
+
+// eventEmitter.on('start', () => {
+//     console.log('started');
+//   });
+  
+// d
+// eventEmitter.emit('start');
+
+// const EventEmitter = require('events');
+
+// const eventEmitter = new EventEmitter();
+
+
+
+// eventEmitter.on('start', () => {
+//     console.log('started');
+//   });
+  
+// d
+// eventEmitter.emit('start');
+
+
+
+
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+
+app.listen('3000', ()=>{
+    console.log("server running on port 3000")
+})
